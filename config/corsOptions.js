35a65219156314121d559988233b81becb4287ee
emre-f <97183433+emre-f@users.noreply.github.com>
2023-02-01 14:05:08 -0500

    lec 2 done

---
+++ config/corsOptions.js
@@ -0,0 +1,15 @@
+const allowedOrigins = require('./allowedOrigins')
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) { // No origin -> Postman etc.
+            callback(null, true) // no error, allowed so true
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true, // Allows the access control allow credentials header
+    optionsSuccessStatus: 200
+}
+
+module.exports = corsOptions
---
+++ middleware/logger.js
@@ -0,0 +1,33 @@
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+const fs = require('fs')
+const fsPromises = require('fs').promises
+const path = require('path')
+
+const logEvents = async (message, logFileName) => {
+    const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss')
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+
+    try {
+
+        // If /logs don't exist, create it
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
+        }   
+        
+        // Add the log item
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFileName), logItem)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// Will log every request that comes in
+// Unless you put some conditions it will fill up really fast, it will run for EVERY request
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
+    console.log(`${req.method} ${req.path}`)
+    next()
+}
+
+module.exports = { logEvents, logger }
