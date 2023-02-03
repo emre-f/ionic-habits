fb3ba9114717a210f00c430cead9dc69fd0570f2
emre-f <97183433+emre-f@users.noreply.github.com>
2023-02-02 20:20:39 -0500

    les3

---
+++ models/Habit.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const habitSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId, // Referring to user type
+            required: true,
+            ref: 'User'
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        value: {
+            type: Number,
+            required: true
+        },
+        notes: {
+            type: String,
+            required: false
+        }
+    },
+    {
+        timestamps: true // Will give us createdAt and updatedAt
+    }
+);
+
+// Will track numbers and assign them
+habitSchema.plugin(AutoIncrement, {
+    inc_field: 'ticket',
+    id: 'ticketNums',
+    start_seq: 0
+})
+
+module.exports = mongoose.model('Habit', habitSchema);
